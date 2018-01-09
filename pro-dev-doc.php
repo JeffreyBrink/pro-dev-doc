8270e0f6f0f208f774288b25f6184c6ac89ada10
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-08 20:19:43 -0700

    created index.css

--- pro-dev-doc.php
+++ pro-dev-doc.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
-<html lang="en"><
+<html lang="en">
 <head>
+	<link rel="stylesheet" type="text/css" href="index.css">
 	<meta charset="utf-8">
 	<title>Jeffrey Brink's professional development document</title>
+	Jeffrey Brink's
 </head>
 <body>
 
