e3cc08f1852e56dfddda4255414b362f772de1d3
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-08 23:03:54 -0700

    final commit of the night.  Fixed margins and bulletts.

--- pro-dev-doc.php
+++ pro-dev-doc.php
@@ -1,23 +1,75 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<link rel="stylesheet" type="text/css" href="index.css">
-	<meta charset="utf-8">
-	<title>Jeffrey Brink's professional development document</title>
-</head>
-<body>
-	<h1>Jeffrey Brink's</h1>
-	<h1>Professional Development</h1>
-	<ul>
-		<li>last updated: December 8th, 2018
-		<li>Career Buddy: Currently accepting applications
-	</ul>
-	<h2>My Links</h2>
-	<div><a href="http://www.linkedin.com/in/jeffreybrink11">Linkedin</a>
-	</div>
-	<div><a href="http://www.twitter.com/JeffreyBrink3">Twitter</a>
-	</div>
-	<h2>Homework Activity</h2>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>JefFrey Brink's Professional Development Document</title>
+	</head>
+	<body>
+		<h1>Jeffrey Brink's </h1>
+		<h1>Professional Development</h1>
+		<p>
+			last updated:  December 8th, 2017
+			<br>Career Buddy:  TBA
+		</p>
 
-</body>
+		<div>
+			<h2>My Links</h2>
+			<ul>
+				<li><a href="http://www.linkedin.com/in/jeffreybrink11">Linkedin</a></li>
+				<li><a href="http://www.twitter.com/JeffreyBrink3">Twitter</a></li>
+			</ul>
+		</div>
+		<div>
+			<h2>Homework Activities</h2>
+		<ul>
+			<li>My learning style</li>
+			<li>40% visual</li>
+			<li>30%auditory</li>
+			<li>30%tactile</li>
+		</ul>
+		</div>
+		<div>
+			<h2>Quantitive Accomplishments</h2>
+			<ul>
+				<li>thing 1</li>
+				<li>thing 2</li>
+				<li>thing 3</li>
+			</ul>
+		</div>
+
+		<div>
+			<h2>Qualitative Features</h2>
+			<ul>
+				<li>thing 1</li>
+				<li>thing 2</li>
+				<li>thing 3</li>
+			</ul>
+		</div>
+		<div>
+			<h2>Golden circle</h2>
+			<ul>
+				<li>what</li>
+				<li>where</li>
+				<li>why</li>
+			</ul>
+		</div>
+		<div>
+			<h2>Elevator Pitch</h2>
+			<ul>
+				<li>version 1</li>
+				<li>words words words</li>
+				<li>version 2</li>
+				<li>words words words</li>
+			</ul>
+		</div>
+		<div>
+			<h2>Weekly reflections</h2>
+			<ul>
+				<li>week 1</li>
+				<li>words words words</li>
+				<li>week 2</li>
+				<li>words words words</li>
+			</ul>
+		</div>
+	</body>
 </html>
