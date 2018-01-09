122ad553023d62e1c6546a67182cae9f440d18e9
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-08 22:33:13 -0700

    this commit is late, caught up in trying to fix spacing.

--- pro-dev-doc.php
+++ pro-dev-doc.php
@@ -8,6 +8,16 @@
 <body>
 	<h1>Jeffrey Brink's</h1>
 	<h1>Professional Development</h1>
+	<ul>
+		<li>last updated: December 8th, 2018
+		<li>Career Buddy: Currently accepting applications
+	</ul>
+	<h2>My Links</h2>
+	<div><a href="http://www.linkedin.com/in/jeffreybrink11">Linkedin</a>
+	</div>
+	<div><a href="http://www.twitter.com/JeffreyBrink3">Twitter</a>
+	</div>
+	<h2>Homework Activity</h2>
 
 </body>
 </html>
